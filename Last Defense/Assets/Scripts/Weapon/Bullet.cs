287575612175df1a298d81563cdc3b7bbec717cd
Erfan Khan <erfan.khan.cse@gmail.com>
2026-05-27 00:41:44 +0600

    Add Target objects, tag and bullet/weapon behavior

--- Last Defense/Assets/Scripts/Weapon/Bullet.cs
+++ Last Defense/Assets/Scripts/Weapon/Bullet.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    // This is a special Unity physics callback function Unity automatically calls it when:
+    // this object collides with another object
     private void OnCollisionEnter(Collision collision)
     {
+        // Did the bullet hit an object tagged "Target"?
         if (collision.collider.CompareTag("Target"))
         {
+            // Gets the object that was hit.
             print("hit " + collision.gameObject.name + " !");
+            
+            // The GameObject this script is attached to.
             Destroy(gameObject);
         }
     }
--- Last Defense/Assets/Scripts/Weapon/Weapon.cs
+++ Last Defense/Assets/Scripts/Weapon/Weapon.cs
@@ -4,16 +4,18 @@ using UnityEngine.InputSystem;
 
 public class Weapon : MonoBehaviour
 {
+    // It stores all your input actions.
     private PlayerControls _controls;
 
     public GameObject bulletPrefab;
-    public Transform bulletSpawn;
+    public Transform bulletSpawn; // This is the location where bullets appear.
 
     public float bulletVelocity;
     public float bulletPrefabLifetime;
 
     private void Awake()
     {
+        // Creates the input system object.
         _controls = new PlayerControls();
     }
 
@@ -37,6 +39,7 @@ public class Weapon : MonoBehaviour
 
     private void FireWeapon()
     {
+        // Instantiate() creates a copy of an object.
         GameObject bullet = Instantiate(
             bulletPrefab,
             bulletSpawn.position,
@@ -44,16 +47,18 @@ public class Weapon : MonoBehaviour
         );
 
         bullet.GetComponent<Rigidbody>().AddForce(
-            bulletSpawn.forward * bulletVelocity,
-            ForceMode.Impulse
+            bulletSpawn.forward * bulletVelocity, // Z = forward/back
+            ForceMode.Impulse // apply instant force one time
         );
 
-        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifetime)
-        );
+        // waits for some time (Coroutine)
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifetime));
     }
 
+    // IEnumerator is what makes it a Coroutine.
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
     {
+        // yield return tells Unity Stop here temporarily, resume later
         yield return new WaitForSeconds(delay);
 
         Destroy(bullet);
